0b24917d4d1145a7e2958d129d554c5efdebb9f2
goguma1000 <102130574+goguma1000@users.noreply.github.com>
2023-07-16 20:43:40 +0900

    폰캣몬.cpp

--- Hash/폰캣몬.cpp
+++ Hash/폰캣몬.cpp
@@ -1,25 +1,14 @@
-#include <string>
 #include <vector>
 #include <map>
-
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    map<string, int> cmap;
-  
-    for (string c : completion) {
-        if (cmap.count(c) != 0) cmap[c] += 1;
-        else cmap.insert({ c,1 });
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    map<int,int> species;
+    for(int i  : nums){
+        if(species.count(i) == 0) species.insert({i,1});
     }
-
-    for (string p : participant) {
-        cmap[p] -=1;
-        if(cmap[p] < 0) {
-            answer = p;
-            break;
-        }
-    }
-    
+    answer = species.size() < nums.size()/2 ? species.size() : nums.size()/2;
     return answer;
 }
