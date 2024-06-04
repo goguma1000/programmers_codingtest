0c2d1b6d814da8d74245e9e211e49b60e28ead3f
goguma1000 <102130574+goguma1000@users.noreply.github.com>
2024-06-04 23:48:02 +0900

    Update 3584[가장 가까운 공통 조상].cpp

--- baekjoon/tree/3584[가장 가까운 공통 조상].cpp
+++ baekjoon/tree/3584[가장 가까운 공통 조상].cpp
@@ -39,3 +39,42 @@ int main() {
 		std::cout << result << "\n";
 	}
 }
+
+//sol 2
+/*
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+
+int main() {
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	int t;
+	std::cin >> t;
+	for (int l = 0; l < t; l++) {
+		int n;
+		std::cin >> n;
+		std::vector<int> parent(n + 1);
+		std::vector<bool> visitied(n + 1);
+		for (int i = 0; i < n - 1; i++) {
+			int p, c;
+			std::cin >> p >> c;
+			parent[c] = p;
+		}
+		int node1, node2;
+		std::cin >> node1 >> node2;
+		while (true) {
+			if (node1 == 0) break;
+			visitied[node1] = true;
+			node1 = parent[node1];
+		}
+		while (true) {
+			if (visitied[node2]) {
+				std::cout << node2 << "\n";
+				break;
+			}
+			node2 = parent[node2];
+		}
+	}
+}
+*/
