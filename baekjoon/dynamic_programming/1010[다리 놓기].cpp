7374592b22c13aed4d4c50e508e0329c2d0368f9
goguma1000 <102130574+goguma1000@users.noreply.github.com>
2024-06-16 21:56:49 +0900

    Create 1010[다리 놓기].cpp

---
+++ baekjoon/dynamic_programming/1010[다리 놓기].cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int main() {
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+	long long t,n,m;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) {
+		std::cin >> n >> m;
+		long long result = 0;
+		if (n == m) {
+			result = 1;
+		}
+		else {
+			long long left = n, right = 1;
+			result = left + right;
+			for (long long i = 2; i < m - n + 1; i++) {
+				left = left * (n - 1 + i) / i;
+				result += left;
+			}
+		}
+		std::cout << result << "\n";
+	}
+	
+	return 0;
+}
