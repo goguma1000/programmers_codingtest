8d5b7fc7a8b387dee90244869107fcbd431c195b
goguma1000 <102130574+goguma1000@users.noreply.github.com>
2024-10-23 02:35:49 +0900

    Create 21939[문제 추천 시스템 Version 1].cpp

---
+++ baekjoon/data_structure/21939[문제 추천 시스템 Version 1].cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <functional>
+#include <unordered_map>
+using namespace std;
+bool minComp(pair<int, int>& a, pair<int, int>& b) {
+	if (a.first == b.second) return a.second > b.second;
+	else return a.first > b.second;
+}
+bool maxComp(pair<int, int>& a, pair<int, int>& b) {
+	if (a.first == b.second) return a.second < b.second;
+	else return a.first < b.second;
+}
+int main() {
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+	unordered_map<int, int> problems; // key: pNum, value: level
+	priority_queue<pair<int, int>, vector<pair<int,int>>,less<pair<int, int>>> maxPQ;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minPQ;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int pNum, lv;
+		cin >> pNum >> lv;
+		problems[pNum] = lv;
+		maxPQ.push({ lv, pNum });
+		minPQ.push({ lv,pNum });
+	}
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		string cmd;
+		cin >> cmd;
+		if (cmd.compare("add") == 0) {
+			int pNum, lv;
+			cin >> pNum >> lv;
+			problems[pNum] = lv;
+			maxPQ.push({ lv, pNum });
+			minPQ.push({ lv, pNum });
+		}
+		else if (cmd.compare("recommend") == 0) {
+			int type;
+			cin >> type;
+			if (type == 1) {
+				while (true) {
+					auto temp = maxPQ.top();
+					if (problems.find(temp.second) != problems.end() && problems[temp.second] == temp.first) {
+						cout << temp.second << '\n';
+						break;
+					}
+					else maxPQ.pop();
+				}
+			}
+			else if (type == -1) {
+				while (true) {
+					auto temp = minPQ.top();
+					if (problems.find(temp.second) != problems.end() && problems[temp.second] == temp.first) {
+						cout << temp.second << '\n';
+						break;
+					}
+					else minPQ.pop();
+				}
+			}
+		}
+		else if(cmd.compare("solved") == 0) {
+			int pNum;
+			cin >> pNum;
+			problems.erase(pNum);
+		}
+	}
+	return 0;
+}
+
